45befa98e9633b52e7afbf82a1fcf1c4ba6ccd28
Vadim <toork1998@gmail.com>
2022-10-13 19:38:28 +0400

    small fixes

--- app/core/DB.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DBNAME', 'piggy_bank');
-define('DBHOST', 'localhost');
-define('DBUSER', 'postgres');
-define('DBPASS', 'admin');
-define('DBPORT', 5432);
-define('DBDRIVER', 'pgsql');
